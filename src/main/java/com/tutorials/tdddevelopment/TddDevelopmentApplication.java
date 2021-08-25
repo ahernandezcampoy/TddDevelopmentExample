5105c9df78898dc90ccbdd3298a43398ff98d849
Alejandro Hernandez Campoy <alejandro.hernandez-campoy@upcnet.es>
2021-08-25 12:41:00 +0200

    Initial commit

---
+++ src/main/java/com/tutorials/tdddevelopment/TddDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.tutorials.tdddevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddDevelopmentApplication.class, args);
+	}
+
+}
