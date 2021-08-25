91e0417ceb594f98e9dc5060f60d59a52acb7662
Alejandro Hernandez Campoy <alejandro.hernandez-campoy@upcnet.es>
2021-08-25 14:59:04 +0200

    AAA pattern showed in comments

--- src/test/java/com/tutorials/tdddevelopment/AccountsTest.java
+++ src/test/java/com/tutorials/tdddevelopment/AccountsTest.java
@@ -10,28 +10,42 @@ class AccountsTest extends TddDevelopmentApplicationTests {
 
     @Test
     void createNewAccountWithZeroBalance() {
+        // Arrange
+
+        // Act
         Account account = new Account();
+
+        // Assert
         Assertions.assertEquals(BigDecimal.ZERO, account.getBalance());
     }
 
     @Test
     void ingress100InEmptyAccountBalanceIs100() {
+        // Arrange
         Account account = new Account();
+
+        // Act
         account.doIngress(new BigDecimal("100.0"));
 
+        // Assert
         Assertions.assertEquals(new BigDecimal("100.0"), account.getBalance());
     }
 
     @Test
     void ingress3000InEmptyAccountBalanceIs3000() {
+        // Arrange
         Account account = new Account();
+
+        // Act
         account.doIngress(new BigDecimal("3000.0"));
 
+        // Assert
         Assertions.assertEquals(new BigDecimal("3000.0"), account.getBalance());
     }
 
     @Test
     void ingress3000WithBalance100MakesAccountBalance3100() {
+        // Arrange
         Account account = new Account();
         /*
             We could had make other solutions to achieve balance=100
@@ -41,8 +55,11 @@ class AccountsTest extends TddDevelopmentApplicationTests {
             Minimum code to achieve the target.
          */
         account.doIngress(new BigDecimal("100.0"));
+
+        // Act
         account.doIngress(new BigDecimal("3000.0"));
 
+        // Assert
         Assertions.assertEquals(new BigDecimal("3100.0"), account.getBalance());
     }
 
